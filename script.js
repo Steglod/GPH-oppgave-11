66ecbec05d6f27d591827f418b811e54b5734aad
Steglod <steffen.glodedata@gmail.com>
2025-12-11 12:51:42 +0100

    deus do

--- script.js
+++ script.js
@@ -1,19 +1,57 @@
 console.log('Hello!');
 
-const deleteButton = document.querySelector('#deleteButton');
-console.log(deleteButton);
+/*
+async function getCat () {
+  try {
 
-function deleteButtonClick() {
-  console.log('knapp Click');
+  }catch {
+    console.error()
+  }
+} */
 
-  const header = document.querySelector('#header');
-  console.log(header);
+const catButton = document.querySelector("#catButton")
+const catInput = document.querySelector("#catInput")
 
-  const main = document.querySelector('#main');
-  console.log(main);
+async function getCat () {
+  try {
+    const catSection = document.querySelector("#catSection")
+    const tag = catInput.value
+    console.log(tag)
+    const data = await fetch(`https://cataas.com/cat/${tag}?json=true`)
+    
+    const response = await data.json()
+    console.log(response)
 
-  header.classList.add('hidden');
-  main.classList.add('hidden');
+    const imageUrl = await response.url;
+    console.log(imageUrl)
+    
+    const img = new Image ()
+      img.src = await imageUrl; // Set the image source
+        img.alt = 'Image from API'; // Set alt text for accessibility
+      catSection.appendChild(img)
+
+  }catch {
+    console.error("No haz burgerz")
+  }
 }
 
-deleteButton.addEventListener('click', deleteButtonClick);
+catButton.addEventListener("click",getCat)
+
+// må sette op html og meir i js og mangler yo mama
+async function  getHoliday() {
+  try{
+    const holidaySection = document.querySelector("#holidaySection")
+    const data = await fetch("https://date.nager.at/api/v3/NextPublicHolidaysWorldwide")
+    const response = await data.json
+    console.log(response)
+
+    response.forEach(function (item) {
+
+      
+    });
+  }catch{
+    consol.error("Ingen helidager lenger :(")
+  }
+}
+
+getHoliday()
